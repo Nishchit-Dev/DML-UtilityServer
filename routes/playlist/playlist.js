79b750da0362ebec9fbcbd75265396119bd14281
nishchit <nishchitpatel84@gmail.com>
2022-04-14 01:05:01 +0530

    sol 2

--- routes/playlist/playlist.js
+++ routes/playlist/playlist.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { runOnChangeOnly } = require('nodemon/lib/config/defaults');
 const router = express.Router();
 const controller = require('../../controllers/playlistController');
 
 router.post('/AddSearch',controller.AddTrackForSearch);
 
-router.post('/newlyadded',controller.NewlyAdded);
+router.post('/newlyAdded',controller.NewlyAdded);
 router.post('/createPlaylist',controller.CreatePlaylist)
 
 router.post('/Add',controller.AddTrack);
