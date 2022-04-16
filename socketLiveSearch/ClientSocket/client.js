101dde8b1e4a9b1a50a06cc3568e946c4c9a913a
nishchit <nishchitpatel84@gmail.com>
2022-04-16 23:54:49 +0530

    fixing socket emit bug

--- socketLiveSearch/ClientSocket/client.js
+++ socketLiveSearch/ClientSocket/client.js
@@ -1,6 +1,6 @@
 const { io } = require("socket.io-client");
 var port = process.env.SocketPort;
-var socket = io("https://dml-server.herokuapp.com/");
+var socket = io("https://dml-server.herokuapp.com:5656");
 
 socket.on("connect", () => {
   // calling server socket input "industry baby"
