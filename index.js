1eb5e2cd8024e20db46a6dc18343a8c85d6b747d
nishchit <nishchitpatel84@gmail.com>
2022-04-23 13:55:15 +0530

    added Allow Cred.... index middle-ware

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   // res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
   next();
 });
