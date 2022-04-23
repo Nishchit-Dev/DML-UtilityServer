051d3ffe15a315211c5260cd201eaeee7226e3a4
nishchit <nishchitpatel84@gmail.com>
2022-04-23 14:05:55 +0530

    enable to set Header from front-end in allow-control-allow-header

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
   res.setHeader("Access-Control-Allow-Credentials", "true");
   // res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
   next();
